a13410d1c4cb61ab568520fb3cddce8466540199
AchroDev <ztblaine@protonmail.com>
2024-03-29 00:13:37 -0500

    Returning the functions "trick" and "treat" to the other function "trickOrTreat". Then I store the return value of the "trickOrTreat" function in variables within the main function.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,22 @@
 // Entry point
 fun main() {
-    // using the :: reference operator to store the "trick" function as a value instead of calling it.
-    //val trickFunction = ::trick
-
-    // Now defining the variable again, without the reference operator as "trick" is now stored in a variable.
-    val trickFunction = trick
-    // Calling the "trick" function normally to get an output.
-    trick()
-    // Displaying the variable now can be called as if it were a function.
+    // Creating the two variables needed to store the return values of the "trickOrTreat" function.
+    val treatFunction = trickOrTreat(false)
+    val trickFunction = trickOrTreat(true)
+    // Calling the two variables above
+    treatFunction()
     trickFunction()
-    //Calling the 'treat' function showing that is works the same as "trick" due to type inference.
-    treat()
 }
 
-// Example "trick function"
-//fun trick() {
-//    println("No treats!")
-//}
+// Define a "trickOrTreat" function and add an if statement that returns the "trick()" function if "isTrick" is true
+// and returns the "treat()" function if "isTrick" is false.
+fun trickOrTreat(isTrick: Boolean): () -> Unit {
+    if (isTrick) {
+        return (trick)
+    } else {
+        return (treat)
+    }
+}
 
 // Example of the function "trick" now being written as a lambda expression. val varName = {body}
 val trick = {
